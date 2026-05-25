e351ac8487e4ac9928280e917e7a15f4b03230e5
Alan <jiglitchialan@gmail.com>
2026-05-25 09:03:55 +0300

    New php file

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+    <head>    
+<title>The Boys</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+    <body>
+        <h3>Welcome to my fanpage The Boys</h3>
+
+        <?php
+echo "works";
+?>
+    </body>
+</html>
